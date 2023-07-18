5008ecfb2c2fb7504bac66f79a744a243b5a430a
Kufupa <wilsonianprogrammer@gmail.com>
2023-07-18 02:50:38 +0100

    added relu activation function

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ import random
 # learning rate is a hyperparameter, default to 0.05
 
 
+def relu(x):
+    return max(0, x)
+
+
 def quadratic(x):
     return x ** 4 + 2 * x ** 3 - 2 * x - 10
 
@@ -22,13 +26,15 @@ def quadratic(x):
 # Generates around 2000 pieces of data, in interval ~ [-10, 20]
 def getRandomQuadraticData():
     data = []
-    for i in range(-10000, 10000, 1):
-        i = i / 5000
+    for i in range(-100000, 100000, 1):
+        i = i / 50000
         if random.random() > 0.9:
-            data.append(quadratic(i))
+            data.append(np.array([i, quadratic(i)]))
     print(f"Data size: {len(data)}")
     return np.array(data)
 
 
 if __name__ == "__main__":
     data = getRandomQuadraticData()
+    np.random.shuffle(data)
+    print(data)
